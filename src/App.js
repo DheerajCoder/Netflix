76793acda7611385d34ab90ab054d0a57436c6b2
Dheeraj Kumar <dheeraj99929@gmail.com>
2024-10-19 17:22:20 +0530

    1 project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold  text-blue-400'>
+
+      This is my setup of create React App 
+    </div>
+  )
+}
+
+export default App
